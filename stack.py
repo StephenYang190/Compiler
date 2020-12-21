64d28247ab12df34674e53ae9d27ebe1531e5925
StephenYang190 <StephenYang@nuaa.edu.cn>
2020-12-21 16:34:36 +0800

    Add stack

---
+++ stack.py
@@ -0,0 +1,15 @@
+class stack:
+    def __init__(self):
+        self.param = []
+        self.last = 0
+
+    def top(self):
+        return self.param[self.last]
+    
+    def push(self, t):
+        self.param.append(t)
+        self.last = self.last + 1
+
+    def pop(self):
+        self.last = self.last - 1
+        return self.param.pop()
