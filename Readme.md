ce9bb3cc1f779a81ed162b48762074c779267ab8
StephenYang190 <StephenYang@nuaa.edu.cn>
2021-01-04 13:45:48 +0800

    Finish the project

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# PL/0编译器
+
+## 要求：
+
+python>=3.6
+
+## 使用
+
+修改Analysis.py文件中的变量来决定路径：
+
+originalCodePath 源代码路径
+
+outputPath 输出路径
+
--- code/Analysis.py
+++ code/Analysis.py
@@ -5,11 +5,13 @@ from interpreter import interpreter
 
 if __name__ == "__main__":
 
-    PascalAnalysis = Analysis('in.txt', 'first.txt', 'follow.txt')
+    originalCodePath = 'in.txt'
+    outputPath = 'out.txt'
+    PascalAnalysis = Analysis(originalCodePath, 'first.txt')
     arg = PascalAnalysis.prog()
 
     if arg['stop'] is not True:
-        f = open('out.txt', 'w')
+        f = open(outputPath, 'w')
         for index, c in enumerate(arg["mcode"]):
             f.write('%d : %s' % (index, c))
             f.write('\n')
